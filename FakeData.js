b1f3225598853efc7c503c859400bd39719fda77
dipanc1 <dipanchhabra@gmail.com>
2022-01-19 14:37:22 +0530

    graphql Nodejs Server Practice

---
+++ FakeData.js
@@ -0,0 +1,38 @@
+const UserList = [{
+        id: 1,
+        name: 'John Doe',
+        username: 'john',
+        age: 30,
+        nationality: 'India'
+    },
+    {
+        id: 2,
+        name: 'John jatt',
+        username: 'johncena',
+        age: 302,
+        nationality: 'tapa desh'
+    },
+    {
+        id: 3,
+        name: 'John singh',
+        username: 'johnaa',
+        age: 3220,
+        nationality: 'punjab'
+    },
+    {
+        id: 4,
+        name: 'John gabru',
+        username: 'john_gabru',
+        age: 130,
+        nationality: 'tapa'
+    },
+    {
+        id: 5,
+        name: 'John di bhen di',
+        username: 'johni_dsnds',
+        age: 20,
+        nationality: 'gurgaon'
+    }
+]
+
+module.exports = { UserList };
